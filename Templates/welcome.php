e1cb32a78a71cf5cc9d0b8b916e0a7239232dc58
DavideCariola <davide.cariola@aulab.it>
2024-02-23 20:26:13 +0200

    inserito bootstrap; creati componenti navbar e footer

--- Templates/welcome.php
+++ Templates/welcome.php
@@ -1,17 +1,32 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Mini-framework</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 
 <body>
+    <?php include_once __DIR__ . '/components/navbar.php'; ?>
 
-    <h1>Hello world!</h1>
-    <a href="/article/create">Inserisci articolo</a>
+    <main class="min-vh-100">
+        <div class="container-fluid p-5 bg-light text-center">
+            <div class="row justify-content-center">
+                <div class="col-12">
+                    <h1 class="display-1">
+                        Il mio super blog
+                    </h1>
+                </div>
+            </div>
+        </div>
+    </main>
 
+    <?php include_once __DIR__ . '/components/navbar.php'; ?>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 
 </html>
