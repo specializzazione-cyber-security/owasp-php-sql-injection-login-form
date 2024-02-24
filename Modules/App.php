072a27c861b9e88a484755ea41f4a9e06a6432ad
DavideCariola <davide.cariola@aulab.it>
2024-02-24 12:59:12 +0200

    rifattorizzato index.php

--- Modules/App.php
+++ Modules/App.php
@@ -2,17 +2,16 @@
 
 namespace App\Modules;
 
-//deve contenere istanza del router e un'istanza del DB oltre alla sessione
 class App
 {
-    public static $root;
     public static App $app;
     public Router $router;
     public Database $database;
     public $session;
 
-    public function __construct($root, Database $database, Router $router)
+    public function __construct(Database $database, Router $router)
     {
+
         $this->database = $database;
         $this->router = $router;
         self::$app = $this;
--- public/index.php
+++ public/index.php
@@ -8,19 +8,33 @@ use App\Modules\Database;
 require_once __DIR__ . "/../vendor/autoload.php";
 require_once __DIR__ . "/../Modules/helpers.php";
 
+//definisco i percorsi dei file
+define('BASE_PATH', __DIR__ . "/../");
+define('CONFIG_PATH', BASE_PATH . "config/");
+define('ROUTES_PATH', BASE_PATH . "routes/");
+
 //recupero il .env indicando il suo path
-$dotenv = Dotenv::createImmutable(__DIR__ . "/../");
+$dotenv = Dotenv::createImmutable(BASE_PATH);
 //crea la superglobal $_ENV
 $dotenv->load();
 
+if (!file_exists(BASE_PATH . '.env')) {
+    die("Il file .env non è stato trovato.");
+}
+
 //recupero le configurazioni del database (dsn, user, psw)
-$db_config = require_once __DIR__ . "/../config/database.php";
+$db_config = require_once CONFIG_PATH . "database.php";
+
+if (empty($db_config)) {
+    die("Configurazione del database non valida.");
+}
+
 $database = new Database($db_config);
 
-$router = require_once __DIR__ . "/../routes/web.php";
+$router = require_once ROUTES_PATH . "web.php";
 
 //istanzio la mia applicazione in modo tale da avere sempre in memoria tutto cio' che mi serve per farla funzionare (database, routing ecc)
-$app = new App(dirname(__DIR__), $database, $router);
+$app = new App($database, $router);
 
 //per sicurezza, una volta che abbiamo configurato il db, cancelliamo queste info
 unset($db_config);
